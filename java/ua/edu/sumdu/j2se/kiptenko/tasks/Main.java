6fae7193c363ca7fffb15da772152aef0e8a256a
Bogdan Kiptenko <bogdankiptenko@gmail.com>
2021-03-19 13:17:16 +0200

    added log4j configuration (Issue #5)

--- java/ua/edu/sumdu/j2se/kiptenko/tasks/Main.java
+++ java/ua/edu/sumdu/j2se/kiptenko/tasks/Main.java
@@ -1,20 +1,19 @@
 package ua.edu.sumdu.j2se.kiptenko.tasks;
 
-import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import ua.edu.sumdu.j2se.kiptenko.tasks.controller.Controller;
 import ua.edu.sumdu.j2se.kiptenko.tasks.model.*;
 import ua.edu.sumdu.j2se.kiptenko.tasks.view.View;
 
-import java.awt.*;
 
 public class Main {
 
     public static final String SAVE_FILE = "data.json";
     private static final Logger logger = Logger.getLogger(Main.class);
 
-    public static void main(String[] args) throws AWTException {
-        BasicConfigurator.configure();
+    public static void main(String[] args){
+        PropertyConfigurator.configure("log4j.properties");
 		AbstractTaskList list = TaskListFactory.createTaskList(ListTypes.types.ARRAY);
         TaskIO.loadFromFile(list, SAVE_FILE);
 		View view = new View();
